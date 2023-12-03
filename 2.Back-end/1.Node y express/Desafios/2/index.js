664aad7930c35a3b4f005f30941dc29d7cbcb98b
Eduardo-Xero <127453979+EA-Xero@users.noreply.github.com>
2023-12-03 17:06:51 -0300

    añadido metodo delete y arreglado metodo put, arreglo de fallo en ids donde se colocaban como string en vez de number

--- 2.Back-end/1.Node y express/Desafios/2/index.js
+++ 2.Back-end/1.Node y express/Desafios/2/index.js
@@ -1,26 +1,45 @@
-const express = require("express")
-const app = express()
-const cors = require("cors")
-const fs = require("fs")
-app.use(cors())
-app.use(express.json())
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const fs = require("fs");
+app.use(cors());
+app.use(express.json());
 
-app.listen(3000,console.log("server running"))
-app.get("/canciones",(req,res)=>{
-    const canciones = JSON.parse(fs.readFileSync("canciones.json"))
-    res.send(canciones)
-})
+app.listen(3000, console.log("server running"));
+app.get("/canciones", (req, res) => {
+  const canciones = JSON.parse(fs.readFileSync("canciones.json"));
+  res.send(canciones);
+});
 
-app.post("/canciones",(req,res)=>{
-    const canciones = JSON.parse(fs.readFileSync("canciones.json"))
-    canciones.push(req.body)
-    fs.writeFileSync("canciones.json",JSON.stringify(canciones))
-    res.send(canciones)
-})
+app.post("/canciones", (req, res) => {
+  const canciones = JSON.parse(fs.readFileSync("canciones.json"));
+  canciones.push(req.body);
+  fs.writeFileSync("canciones.json", JSON.stringify(canciones));
+  res.send(canciones);
+});
 
-app.put("/canciones/:id",(req,res)=>{
-    const canciones = JSON.parse(fs.readFileSync("canciones.json"))
-    canciones[req.params.id] = req.body
-    fs.writeFileSync("canciones.json",JSON.stringify(canciones))
-    res.send(canciones)
-})
+app.put("/canciones/:id", (req, res) => {
+  const id = parseInt(req.params.id); // Convertir id a tipo numérico
+  const canciones = JSON.parse(fs.readFileSync("canciones.json"));
+  const cancionActualizadaIndex = canciones.findIndex((c) => c.id === id);
+  if (cancionActualizadaIndex !== -1) {
+    canciones[cancionActualizadaIndex] = req.body;
+    fs.writeFileSync("canciones.json", JSON.stringify(canciones));
+    res.send(canciones);
+  } else {
+    res.status(404).send("Canción no encontrada");
+  }
+});
+
+app.delete("/canciones/:id", (req, res) => {
+  const id = parseInt(req.params.id); // Convertir id a tipo numérico
+  const canciones = JSON.parse(fs.readFileSync("canciones.json"));
+  const cancionEliminadaIndex = canciones.findIndex((c) => c.id === id);
+  if (cancionEliminadaIndex !== -1) {
+        canciones.splice(cancionEliminadaIndex, 1);
+        fs.writeFileSync("canciones.json", JSON.stringify(canciones));
+        res.send(canciones);
+  } else {
+    res.status(404).send("Canción no encontrada");
+  }
+});
