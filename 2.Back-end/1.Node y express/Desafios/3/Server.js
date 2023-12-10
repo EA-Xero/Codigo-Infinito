24c0005d4f1db5df3355fe15162e602c6efdfcd3
Eduardo-Xero <127453979+EA-Xero@users.noreply.github.com>
2023-12-10 19:24:49 -0300

    funcionamiento principal de la app listo

--- 2.Back-end/1.Node y express/Desafios/3/Server.js
+++ 2.Back-end/1.Node y express/Desafios/3/Server.js
@@ -14,3 +14,15 @@ app.get("/posts",async (req,res)=>{
     const posts = await leerposts()
     res.json(posts)
 })
+
+/* app.post("./posts",async (req,res)=>{
+    const { titulo, img, descripcion, likes } = req.body
+    await newpost( titulo, img, descripcion, likes )
+    res.send("Post agregado con exito")
+}) */
+
+app.post("/posts", async (req, res) => {
+    const { titulo, img, descripcion, likes } = req.body;
+    await newpost(titulo, img, descripcion, likes);
+    res.send("Post agregado con éxito");
+  });
