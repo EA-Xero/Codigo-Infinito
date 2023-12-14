cee53a8e3e3f204152b8b19f2afdac289a4d3802
Eduardo-Xero <127453979+EA-Xero@users.noreply.github.com>
2023-12-14 17:16:07 -0300

    trabajando en el proyecto anterior añadiendo sentencias try-catch y agregando el metodo delete

--- 2.Back-end/1.Node y express/Desafios/3/consultas.js
+++ 2.Back-end/1.Node y express/Desafios/3/consultas.js
@@ -13,14 +13,20 @@ const newpost = async (titulo,url,descripcion,likes) =>{
     const consulta = "insert into posts values (Default,$1,$2,$3,$4)"
     const values = [titulo,url,descripcion,likes]
     const result = await pool.query(consulta,values)
-    console.log("Post agregado")
-    console.log("Datos recibidos en newpost:", titulo, url, descripcion, likes);
 }
 
 const leerposts = async()=>{
     const {rows} = await pool.query("select * from posts")
-    console.log(rows)
     return rows
 }
 
-module.exports = {newpost,leerposts}
+const eliminarpost = async(id) =>{
+        const consulta = "delete from posts where id = $1"
+        const values = [id]
+        const {rowCount} = await pool.query(consulta,values)
+        if (rowCount === 0){
+            throw {code:404,message:"No se encuentra un posts con el id asociado"}
+        }
+}
+
+module.exports = {newpost,leerposts,eliminarpost}
